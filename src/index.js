703518eea17840386eba8e50e05c97e68de611fa
Aleksandr Spiridonov <spiridonov.xander@gmail.com>
2023-01-16 17:25:08 +0300

    first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let n1 = n
+    if (n1 < 0) {
+        n1 *= -1
+    }
+    let n2 = '';
+
+    n2 = Number(String(n1).split('').reverse().join('')) 
+  return n2
 }
