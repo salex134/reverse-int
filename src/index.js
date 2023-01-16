e75adfa1439580208bf47f8cd41c7bc84cc19742
Aleksandr Spiridonov <spiridonov.xander@gmail.com>
2023-01-16 17:44:13 +0300

    matematical method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 module.exports = function reverse (n) {
-    let n1 = n
-    if (n1 < 0) {
-        n1 *= -1
+    let n1 = Math.abs(n);
+    num = 0;
+    while (n1 > 0) {
+        num = num * 10 + n1 % 10
+        n1 = Math.floor(n1 /= 10)
     }
+    return num
+}
+
+
+
+/* module.exports = function reverse (n) {
+    let n1 = Math.abs(n);
     let n2 = '';
 
     n2 = Number(String(n1).split('').reverse().join('')) 
   return n2
 }
+
+
+reverse(-5901)
+
+ */
